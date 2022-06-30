ce2339420fb649f413270f7d7bb5a8915577f436
gavvahar <nihargavva@gmail.com>
2022-06-30 14:46:13 -0400

    finished with this

--- JS/index.js
+++ JS/index.js
@@ -1,34 +1,37 @@
 function doWork() {
-    let start = new Date().getTime()
-    let now = start;
-    while (now < start + 500) {
-        now = new Date().getTime()
-    }
+  return new Promise((resolve) => {
+    const myWorker = new Worker("worker.js");
+    myWorker.postMessage("doWork");
+    myWorker.onmessage = function () {
+      console.log("in here");
+      resolve();
+    };
+  });
 }
 
 function sleep(time) {
-    return new Promise(resolve => setTimeout(resolve, time))
+  return new Promise((resolve) => setTimeout(resolve, time));
 }
 
-let example1Button = document.querySelector('#example-1')
-let example2Button = document.querySelector('#example-2')
-let example3Button = document.querySelector('#example-3')
+let exampleButton = [
+  document.querySelector("#example-1"),
+  document.querySelector("#example-2"),
+  document.querySelector("#example-3"),
+];
 
-example1Button.addEventListener('click', function () {
-    exampleOne()
-    example1Button.textContent = 'Done!'
-})
+exampleButton[0].addEventListener("click", function () {
+  exampleOne();
+  exampleButton[0].textContent = "Done!";
+});
 
-example2Button.addEventListener('click', function () {
-    exampleTwo()
-        .then(function () {
-            example2Button.textContent = 'Done!'
-        })
-})
+exampleButton[1].addEventListener("click", function () {
+  exampleTwo().then(function () {
+    exampleButton[1].textContent = "Done!";
+  });
+});
 
-example3Button.addEventListener('click', function () {
-    Promise.all(exampleThree())
-        .then(function(){
-            example3Button.textContent = 'Done!'
-        })
-})
+exampleButton[2].addEventListener("click", function () {
+  Promise.all(exampleThree()).then(function () {
+    exampleButton[2].textContent = "Done!";
+  });
+});
